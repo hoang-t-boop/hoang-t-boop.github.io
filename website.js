ba0ef2163645cb72cbbd7d785d9daaf5f08d66e5
hoang-t-boop <52755278+hoang-t-boop@users.noreply.github.com>
2019-07-10 09:52:29 -0700

    Add files via upload

---
+++ website.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<Title>JavaScript Alert</Title>
+<button onload="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert("I am an alert box!");
+}
+</script>
